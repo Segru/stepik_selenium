dfee9ba625caf30ab9afb6888aaf533d26f4b51e
Sergey <s.gruchik@ispsystem.com>
2020-08-27 13:09:48 +0800

    change test_items.py

--- pytest_lesson36_step9/test_items.py
+++ pytest_lesson36_step9/test_items.py
@@ -1,6 +1,12 @@
+import time
+from selenium import webdriver
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_find_button_to_add_to_basket(browser):
     browser.get(link)
-    browser.find_element_by_css_selector(".btn-add-to-basket")
+    button = browser.find_element_by_css_selector(".btn-add-to-basket")
+    button.click()
+    assert button, "Button not found"
+    time.sleep(1)
